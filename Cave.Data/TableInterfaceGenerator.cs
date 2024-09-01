60c97d711398493d204359b36fbddbe91b98148e
Andreas Rohleder <39777727+Dingsd4@users.noreply.github.com>
2024-09-01 14:43:43 +0200

    TableInterfaceGenerator allows multi line comments (#7)

--- Cave.Data/TableInterfaceGenerator.cs
+++ Cave.Data/TableInterfaceGenerator.cs
@@ -75,6 +75,7 @@ public class TableInterfaceGenerator
         code.AppendLine("//-----------------------------------------------------------------------");
         code.AppendLine();
         code.AppendLine("using System;");
+        code.AppendLine("using System.ComponentModel;");
         code.AppendLine("using System.Globalization;");
         code.AppendLine("using System.CodeDom.Compiler;");
         code.AppendLine("using Cave.Data;");
@@ -136,7 +137,7 @@ public class TableInterfaceGenerator
             code.AppendLine($"\t\t/// <summary>{field}</summary>");
             if (!string.IsNullOrEmpty(field.Description))
             {
-                var description = $"{field.Description}".Replace("&", "&amp;").Replace("<", "&gt;").Replace(">", "&lt;").Replace("\"", "&quot;");
+                var description = $"{field.Description}".Replace("&", "&amp;").Replace("<", "&gt;").Replace(">", "&lt;").Replace("\"", "&quot;").ReplaceNewLine("<br/>");
                 code.AppendLine($"\t\t/// <remarks>{description}</remarks>");
                 code.AppendLine($"\t\t[Description(\"{field.Description.EscapeUtf8()}\")]");
             }
